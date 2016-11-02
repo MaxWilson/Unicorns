84835dfcd11480ba112f2f8b22e12021ad1c4a71
Max Wilson <maxw@microsoft.com>
2016-11-01 18:41:59 -0700

    Initial project

---
+++ main.fs
@@ -0,0 +1,30 @@
+﻿module TeaganSquish.Main
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import.PIXI
+open Fable.Import.Browser
+
+let render = Globals.autoDetectRenderer(window.innerHeight, window.innerWidth, [BackgroundColor (float 0x1099bb); Resolution 1.])
+             |> unbox<SystemRenderer>
+
+let gameDiv = document.getElementById("content")
+gameDiv.appendChild(render.view) |> ignore
+
+let stage = Container()
+let pic = Sprite.fromImage("unicorn.png")
+pic.anchor.x <- 0.5
+pic.anchor.y <- 0.5
+pic.position.x <- -1000.
+pic.position.y <- 300.
+stage.addChild(pic) |> ignore
+
+let rec animate(dt:float) =
+    window.requestAnimationFrame(FrameRequestCallback animate) |> ignore
+    pic.position.x <- (pic.position.x + 5.)
+    if pic.position.x > 1800. then
+        pic.position.x <- -1000.
+    render.render(stage)
+
+animate 0.
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var path = require("path");
+var webpack = require("webpack");
+
+var cfg = {
+  entry: [
+    "./js/main.js"
+  ],
+  output: {
+    path: path.join(__dirname, "publish"),
+    filename: "bundle.js"
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('production')
+      }
+    })
+  ],
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "source-map-loader"
+      }
+    ]
+  }
+};
+
+module.exports = cfg;
