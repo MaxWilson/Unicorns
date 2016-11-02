acba830e7edc9930d60063283a495ece49d211bf
Max Wilson <wilson.max@gmail.com>
2016-11-02 07:33:22 -0700

    Play with text

--- main.fs
+++ main.fs
@@ -3,8 +3,8 @@
 open System
 open Fable.Core
 open Fable.Core.JsInterop
-open Fable.Import.PIXI
 open Fable.Import.Browser
+open Fable.Import.PIXI
 
 let render = Globals.autoDetectRenderer(window.innerHeight, window.innerWidth, [BackgroundColor (float 0x1099bb); Resolution 1.])
              |> unbox<SystemRenderer>
@@ -13,9 +13,12 @@ let gameDiv = document.getElementById("content")
 gameDiv.appendChild(render.view) |> ignore
 
 let stage = Container()
-let pic = Sprite.fromImage("Unicorn.png")
-pic.anchor.x <- 0.5
-pic.anchor.y <- 0.5
+let pic = Container()
+let p = Sprite.fromImage("Unicorn.png")
+p.anchor.x <- 0.5
+p.anchor.y <- 0.5
+pic.addChild(p) |> ignore
+pic.addChild(Text("captcha!",[TextStyle.Align "center"; TextStyle.Font "bold italic 80px"; TextStyle.Fill (U2.Case1 "blue")])) |> ignore
 pic.position.x <- -600.
 pic.position.y <- 300.
 stage.addChild(pic) |> ignore
@@ -30,7 +33,7 @@ let countingText =
         TextStyle.StrokeThickness 7.
         ])
 countingText.position.x <- 310.
-countingText.position.y <- 320.
+countingText.position.y <- 620.
 countingText.anchor.x <- 0.5
 stage.addChild(countingText) |> ignore
 
